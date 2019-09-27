839ed760e48ca7b6d88c37e0863aa24eadb76092
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2019-09-27 13:35:03 +0200

    Building Custom Obs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 
-import { interval, Subscription } from "rxjs";
+import { interval, Subscription, Observable } from "rxjs";
 
 @Component({
   selector: "app-home",
@@ -15,9 +15,31 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-    this.firtsObsSubs = this.secondsCounter.subscribe(count => {
-      console.log(count);
+    // this.firtsObsSubs = this.secondsCounter.subscribe(count => {
+    //   console.log(count);
+    // });
+
+    const customIntervalObs = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 5) observer.complete();
+        if (count > 3) observer.error(new Error("Error"));
+        count++;
+      }, 1000);
     });
+
+    this.firtsObsSubs = customIntervalObs.subscribe(
+      data => {
+        console.log(data);
+      },
+      err => {
+        alert(err.message);
+        console.log(err);
+      }, () => {
+        console.log("Completed");
+      }
+    );
   }
 
   ngOnDestroy(): void {
