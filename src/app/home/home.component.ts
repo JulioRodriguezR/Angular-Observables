c16adcc0f821914907d5824e062e5407ecfeaf7f
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2019-09-27 13:30:23 +0200

    Getting Closer to the Core of Obs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+
+import { interval, Subscription } from "rxjs";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  secondsCounter = interval(1000);
+
+  private firtsObsSubs: Subscription;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.firtsObsSubs = this.secondsCounter.subscribe(count => {
+      console.log(count);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.firtsObsSubs.unsubscribe();
+  }
 }
