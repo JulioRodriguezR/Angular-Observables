b18cd530baa7160c645530c51c70330016203208
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2019-09-29 23:46:51 +0200

    Subjects1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { UserService } from "./user.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  userActivate = false;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    /* 
+      + Configurar oyente
+        
+        Poder subcribirnos con el servicio del usuario, 
+        el emisor activado y subscribirnos. Obteniendo nuestra activación.
+    */
+    this.userService.activatedEmitter.subscribe(didActivate => {
+      this.userActivate = didActivate;
+    });
+  }
 }
