bfac08dbd6254727925c9de281b8d0e2ad5d23e8
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2019-09-29 19:35:28 +0200

    Operators

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 
 import { interval, Subscription, Observable } from "rxjs";
+import { map, filter } from "rxjs/operators";
 
 @Component({
   selector: "app-home",
@@ -15,10 +16,6 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-    // this.firtsObsSubs = this.secondsCounter.subscribe(count => {
-    //   console.log(count);
-    // });
-
     const customIntervalObs = Observable.create(observer => {
       let count = 0;
       setInterval(() => {
@@ -29,17 +26,28 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.firtsObsSubs = customIntervalObs.subscribe(
-      data => {
-        console.log(data);
-      },
-      err => {
-        alert(err.message);
-        console.log(err);
-      }, () => {
-        console.log("Completed");
-      }
-    );
+    this.firtsObsSubs = customIntervalObs
+      .pipe(
+        // Operators
+        filter(data => {
+          return data > 0;
+        }),
+        map((data: number) => {
+          return "Round:" + (data + 1);
+        })
+      )
+      .subscribe(
+        data => {
+          console.log(data);
+        },
+        err => {
+          alert(err.message);
+          console.log(err);
+        },
+        () => {
+          console.log("Completed");
+        }
+      );
   }
 
   ngOnDestroy(): void {
